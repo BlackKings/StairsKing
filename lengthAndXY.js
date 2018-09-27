d7dc3ce2af9c9959ddd3d298e7687cf17ce77e66
greenking <1014589302@qq.com>
2018-09-27 17:39:38 +0800

    test

---
+++ lengthAndXY.js
@@ -0,0 +1,43 @@
+var sAdderSize = 11;
+
+//
+function adderList(tLength, tWidth) {
+    var
+        sLength = tLength / sAdderSize,
+        sWidth = tWidth,
+        test = testKing(sLength, sWidth)
+
+    return test;
+}
+
+
+//设置 x y 坐标
+function testKing(sLength, sWidth) {
+    var
+        sAdderX = sLength / 2,
+        sAdderY = sWidth / 2,
+        s = [];
+    for (let i = 0; i < sAdderSize; i++) {
+        s.push([Math.round(sAdderX), Math.round(sAdderY)]);
+        sAdderX += sLength;
+    }
+    return s;
+}
+
+
+function dowAdder(tLength, tWidth, times) {
+    var
+        test1 = tLength,
+        test2 = tWidth,
+        aaa = [];
+    for (let i = 0; i < times; i++) {
+        let bb = []
+        bb = adderList(test1, test2);
+        aaa.push(bb);
+        test1 *= 0.89;
+        test2 *= 0.89;
+    }
+    return aaa;
+}
+var kingtest = dowAdder(1000, 80, 8);
+console.log(kingtest[1]  +"\n" + kingtest[kingtest.length -1]);
--- test.ts
+++ test.ts
@@ -1,6 +1,16 @@
-function green(person) {
-    return "Hello " + person;
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public inName: string, public lastName: string) {
+        this.fullName = firstName + " " + inName + " " + lastName;
+    }
+}
+interface Person {
+    firstName: string;
+    lastName: string;
 }
 
-var king = "greenking";
-console.log(green(king));
+function greeter(person: Person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+let user = new Student("Green", ".D.", "King");
+document.body.innerHTML = greeter(user);
